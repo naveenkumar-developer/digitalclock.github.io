08c39f913bf70d2ba16170203d2a5e3724c137fb
NAVEENKUMAR.C <125805333+naveenkumar-developer@users.noreply.github.com>
2023-02-21 12:43:47 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -13,16 +13,16 @@ let z = x.getHours();
 
 //backgroundimage dynamically changing on sun rise to sun set
 if (z < 12) {
-  bgimg.style.backgroundImage = "url('../digiclock/images/morning.png')";
+  bgimg.style.backgroundImage = "url('./images/morning.png')";
   bgimg.style.backgroundSize = "cover";
 } else if (z < 16) {
-  bgimg.style.backgroundImage = "url('../digiclock/images/afternoon.jpg')";
+  bgimg.style.backgroundImage = "url('./images/afternoon.jpg')";
   bgimg.style.backgroundSize = "cover";
 } else if (z < 19) {
-  bgimg.style.backgroundImage = "url('../digiclock/images/evening.jpg')";
+  bgimg.style.backgroundImage = "url('./images/evening.jpg')";
   bgimg.style.backgroundSize = "cover";
 } else {
-  bgimg.style.backgroundImage = "url('../digiclock/images/night.png')";
+  bgimg.style.backgroundImage = "url('./images/night.png')";
   bgimg.style.backgroundSize = "cover";
 }
 
